83b27b1f915a09cf31dbbf1fcd0d8e04b4217f08
Tom <tom.terado@gmail.com>
2019-03-04 18:15:36 +0900

    First Commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config({ path: 'variables.env' });
+
+onst express = require('express');
+const bodyParser = require('body-parser');
+const verifyWebhook = require('./verify-webhook');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(5000, () => console.log('Express server is listening on port 5000'));
+
+app.get('/', verifyWebhook);
